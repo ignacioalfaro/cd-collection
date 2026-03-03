9d5b538acf0ea826f99b7a28c86c30d187bafab4
ignacioalfaro <103149650+ignacioalfaro@users.noreply.github.com>
2026-03-03 08:16:20 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "cd-collection-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
